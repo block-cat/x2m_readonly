8e9d90de3736de4abfb3d2c2e113fa59fa57ab55
Kevin Kong <kfx2007@163.com>
2019-06-08 08:09:20 +0000

    main

---
+++ README.md
@@ -0,0 +1,17 @@
+# X2many Readonly
+
+in some special situation, some people wants to make one2many/many2many fields readonly when edit in form view/tree view. This module is just writing for those who sucks in this situation.
+
+某些情况下，我们希望针对x2many类型的字段，在编辑的时候做到只读。遗憾的是，odoo目前的原生模块中并没有此类的操作可供选择。对于x2many类型的字段，哪怕你声明了readonly，单击条目依旧能够弹窗。而且，对于声明了readonly的字段是不能保存到数据库中的。
+
+## usage
+
+after installing this module , just add context keyword in your fields declaration, it looks like this:
+
+用法：
+
+在字段声明中，加入context，声明x2m_readonly属性为true:
+
+```python
+    x2m = fields.One2many(...,context="{'x2m_readonly':True}")
+```
---
+++ static/src/js/xmr.js
@@ -0,0 +1,91 @@
+odoo.define("orssica_sale.sale_form", function (require) {
+    'use strict';
+
+    //继承自web view_dialog
+    var ViewDialog = require("web.view_dialogs");
+    var RelatedField = require("web.relational_fields");
+
+    var One2manyField = RelatedField.FieldOne2Many;
+
+    var FormViewDialog = ViewDialog.FormViewDialog;
+
+    FormViewDialog.include({
+        open: function () {
+            // 添加xm_readonly属性不弹窗
+            if (!this.context.xm_readonly) {
+                // 没有设置xm_readonly属性
+                this._super();
+            }
+        },
+    });
+
+    One2manyField.include({
+
+        _onEditLine:function(ev){
+
+            // xm_readonly 不予响应编辑事件
+            if(!this.field.context.xm_readonly){
+                this._super(ev)
+            }
+            
+        },
+
+        _render: function () {
+            if (!this.view) {
+                return this._super();
+            }
+            if (this.renderer) {
+                this.currentColInvisibleFields = this._evalColumnInvisibleFields();
+                this.renderer.updateState(this.value, { 'columnInvisibleFields': this.currentColInvisibleFields });
+                this.pager.updateState({ size: this.value.count });
+                return $.when();
+            }
+            var arch = this.view.arch;
+            var viewType;
+            var rendererParams = {
+                arch: arch,
+            };
+
+        
+            // 对于拥有xm_readonly属性的字段，不显示添加和删除按钮
+            if (this.field.context.xm_readonly){
+                this.activeActions.create = false;
+                this.activeActions.delete = false;
+            }
+
+            if (arch.tag === 'tree') {
+                viewType = 'list';
+                this.currentColInvisibleFields = this._evalColumnInvisibleFields();
+                _.extend(rendererParams, {
+                    editable: this.mode === 'edit' && arch.attrs.editable,
+                    addCreateLine: !this.isReadonly && this.activeActions.create,
+                    addTrashIcon: !this.isReadonly && this.activeActions.delete,
+                    isMany2Many: this.isMany2Many,
+                    columnInvisibleFields: this.currentColInvisibleFields,
+                });
+            }
+
+            if (arch.tag === 'kanban') {
+                viewType = 'kanban';
+                var record_options = {
+                    editable: false,
+                    deletable: false,
+                    read_only_mode: this.isReadonly,
+                };
+                _.extend(rendererParams, {
+                    record_options: record_options,
+                });
+            }
+
+            _.extend(rendererParams, {
+                viewType: viewType,
+            });
+            var Renderer = this._getRenderer();
+            this.renderer = new Renderer(this, this.value, rendererParams);
+
+            this.$el.addClass('o_field_x2many o_field_x2many_' + viewType);
+            return this.renderer ? this.renderer.appendTo(this.$el) : this._super();
+        },
+        
+    });
+});
